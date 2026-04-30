a3fa9da376297499c019350309f7320e4b1786fd
jnnilsson <jn.jensnilsson@gmail.com>
2026-04-30 15:16:46 +0200

    initial commit, setup

---
+++ src/main/java/com/mythicmarket/cardmarketshipping/CardmarketShippingApplication.java
@@ -0,0 +1,13 @@
+package com.mythicmarket.cardmarketshipping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardmarketShippingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardmarketShippingApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/mythicmarket/cardmarketshipping/config/PostNordConfig.java
@@ -0,0 +1,33 @@
+package com.mythicmarket.cardmarketshipping.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "postnord")
+@Data
+public class PostNordConfig {
+
+    private String apiKey;
+    private String issuerCode;
+    private String customerNumber;
+
+    private ServiceCodes serviceCodes;
+    private Sender sender;
+
+    @Data
+    public static class ServiceCodes {
+        private String domestic;
+        private String international;
+    }
+
+    @Data
+    public static class Sender {
+        private String name;
+        private String street;
+        private String zipCode;
+        private String city;
+    }
+
+}
