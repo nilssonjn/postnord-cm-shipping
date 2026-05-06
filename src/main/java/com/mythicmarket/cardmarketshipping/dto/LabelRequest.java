15cc346bbe886b5aad06f5aca647d66b371cdb50
jnnilsson <jn.jensnilsson@gmail.com>
2026-05-06 15:36:39 +0200

    refactor LabelRequest to use record for improved data handling

--- src/main/java/com/mythicmarket/cardmarketshipping/dto/LabelRequest.java
+++ src/main/java/com/mythicmarket/cardmarketshipping/dto/LabelRequest.java
@@ -1,16 +1,13 @@
 package com.mythicmarket.cardmarketshipping.dto;
 
-import lombok.Data;
-
-@Data
-public class LabelRequest {
-
-    private String buyerName;
-    private String street;
-    private String postalCode;
-    private String city;
-    private String countryName;
-    private String orderId;
-    private int weightGrams;
-    private String serviceType;
+public record LabelRequest(
+        String buyerName,
+        String street,
+        String postalCode,
+        String city,
+        String countryName,
+        String orderId,
+        int weightGrams,
+        String serviceType
+) {
 }
